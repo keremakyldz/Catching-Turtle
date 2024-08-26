d7952f12349282557b2906db099f4fad5fe080f1
Mustafa Kerem Akyıldız <mkeremakyldz[200~@gmail.com>
2024-08-27 00:06:00 +0200

    Stop game function added, There was a problem that cause multiple turtle spawn that bug fixed, unnecessary code removed

--- main.py
+++ main.py
@@ -38,18 +38,25 @@ def timer():
     else:
         time_turtle.clear()
         time_turtle.write("Game over!",align="center",font=("Arial",24,"normal"))
+        stop_game()
+#Function to stop the game
+def stop_game():
+    for t in turtles:
+        t.hideturtle()
+    turtles.clear()
 #Creating a turtle to catch
 turtles = []
 def random_turtle():
-    turtle_instance = turtle.Turtle("turtle")
-    turtle_instance.color("green")
-    turtle_instance.penup()
-    x = random.randint(-turtle_screen.window_width()//2, turtle_screen.window_width()//2)
-    y = random.randint(-turtle_screen.window_height()//2, turtle_screen.window_height()//2)
-    turtle_instance.goto(x,y)
-    turtles.append(turtle_instance)
-    turtle_instance.onclick(lambda x, y:click_turtle(turtle_instance))
-    turtle_screen.ontimer(lambda: delete_turtle(turtle_instance),1000)
+    if countdown > 0:
+        turtle_instance = turtle.Turtle("turtle")
+        turtle_instance.color("green")
+        turtle_instance.penup()
+        x = random.randint(-turtle_screen.window_width()//2, turtle_screen.window_width()//2)
+        y = random.randint(-turtle_screen.window_height()//2, turtle_screen.window_height()//2)
+        turtle_instance.goto(x,y)
+        turtles.append(turtle_instance)
+        turtle_instance.onclick(lambda x, y:click_turtle(turtle_instance))
+        turtle_screen.ontimer(lambda: delete_turtle(turtle_instance),1000)
 #Code for clicking the turtles randomly created
 def click_turtle(r):
     global score
@@ -59,16 +66,17 @@ def click_turtle(r):
         turtles.remove(r)
         score += 1
         score_update()
+        random_turtle()
 #Code for deleting turtles that randomly created
 def delete_turtle(r):
     if r.isvisible():
         r.hideturtle()
         r.clear()
+        turtles.remove(r)
         random_turtle()
 
-# Amount of turtles randomly created
-for i in range(10):
-    random_turtle()
+random_turtle()
+
 #Turtle that controlled by mouse
 mouse = turtle.Turtle()
 mouse.hideturtle()
@@ -78,9 +86,9 @@ def mouse_function(x,y):
     mouse.goto(x,y)
     mouse.ondrag(mouse_function)
     mouse.penup()
+
 mouse.speed("fastest")
 mouse.ondrag(mouse_function)
 #end codes
 timer()
 turtle.mainloop()
-turtle_screen.mainloop()
